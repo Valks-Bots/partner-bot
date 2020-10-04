ef6b291c579deec3ed461c415760b96b8b02e8a0
valkyrienyanko <sebastianbelle074@gmail.com>
2020-10-04 11:33:48 -0500

    Removed copyright notice (forgot about this sorry)
    I forgot to remove this.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-/*
- * Copyright © 2019 valkyrienyanko All rights reserved.
- *
- * You may not, unless given explicit permission from myself, make a profit off the following source or modify or remove the author name.
- */
 if (Number(process.version.slice(1).split('.')[0]) < 8) throw new Error('Node 8.0.0 or higher is required. Update Node on your system.')
 
 const Discord = require('discord.js')
@@ -30,8 +25,7 @@ const init = async () => {
   console.clear()
 
   console.log('------------------------------------------------------------------------')
-  console.log('If you need any help, you can talk to me through discord at valk#3277')
-  console.log('You can also email me at sebastianbelle074@gmail.com')
+  console.log('You may use this bot in anyway you see fit, you may even change the owner name to be your own.')
   console.log('------------------------------------------------------------------------')
 
   const loader = await client.loader
